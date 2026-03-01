35a4f759abd103466a2a3deb21030b01162474bb
Auto Tester <evalareva17@gmail.com>
2026-03-02 01:33:35 +0200

    Практика написания Pydantic-моделей

---
+++ pydantic_create_user.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel, Field, EmailStr
+
+
+class UserSchema(BaseModel):
+    """
+    Модель данных пользователя.
+    """
+    id: str
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+
+class CreateUserRequestSchema(BaseModel):
+    """
+    Модель запроса на создание пользователя.
+    """
+    email: EmailStr
+    password: str
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+
+
+class CreateUserResponseSchema(BaseModel):
+    """
+    Модель ответа успешного создания пользователя.
+    """
+    # В ответе приходит объект "user", который соответствует структуре UserSchema
+    user: UserSchema
